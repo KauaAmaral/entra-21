8db712ec646dfcee4d73886a5be0b0f5631fd0bd
Guilherme Dzesigaleki Mueller <guilhermemuellerdzesigaleski@gmail.com>
2022-05-13 21:32:59 -0300

    Exercicio01 Concluido Exerrcicio 01 feito em sala de aula

---
+++ Entra21.ExerciciosForTry/Exercicio01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosForTry
+{
+    internal class Exercicio01
+    {
+        public void Executar()
+        {
+            // Solicite o preço e nome da peça de 13 peças.
+
+            for (var i = 0; i < 13; i++)
+            {
+                Console.WriteLine("Nome da peça: ");
+                var nomePeca = Console.ReadLine();
+
+                var valorPeca = 0.0;
+
+                while (valorPeca <= 0)
+                    try
+                    {
+                        Console.WriteLine("Valor da peça: ");
+                        valorPeca = Convert.ToDouble(Console.ReadLine());
+                        Console.Clear();
+
+                        if (valorPeca <= 0)
+                        {
+                            Console.WriteLine("Valor não pode ser menor ou igual a 0");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Valor digitado não é um inteiro.");
+                    }
+            }
+
+        }
+    }
+}
---
+++ Entra21.ExerciciosForTry/Program.cs
@@ -0,0 +1,15 @@
+﻿using Entra21.ExerciciosForTry;
+
+Console.WriteLine(@"--------Menu-------
+1- Exercício 01
+");
+
+Console.Write("Digite a opção desejada: ");
+int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+
+if (opcaoDesejada == 1)
+{
+    Exercicio01 exercicio01 = new Exercicio01();
+    exercicio01.Executar();
+}
