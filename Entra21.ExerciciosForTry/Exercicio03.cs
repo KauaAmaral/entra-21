4954e4ad698df2947906516d1c8eb78c16491c92
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-14 10:40:30 -0300

    Exercicio03 Completo Exercicio 03 concluido em casa

---
+++ Entra21.ExerciciosForTry/Exercicio03.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosForTry
+{
+    internal class Exercicio03
+    {
+        public void Executar()
+        {
+            // Solicite um número e apresentar a tabuada do mesmo até 1000.
+
+            var tabuada = 0;
+            var i = 1000;
+
+            while (tabuada <= 0)
+            {
+                try
+                {
+                    Console.Write("Digite qual tabuada deseja: ");
+                    tabuada = Convert.ToInt32(Console.ReadLine());
+
+                    if (tabuada <= 0)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(@"Apenas maiores que 0.
+                        ");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Clear();
+                    Console.WriteLine(@"Apenas números inteiro.
+                    ");
+                }
+            }
+
+            for (var j = 1; j <= i; j++)
+            {
+                var resultado = j * tabuada;
+                Console.WriteLine(j + " x " + tabuada + " = " + resultado);
+            }
+        }
+    }
+}
--- Entra21.ExerciciosForTry/Program.cs
+++ Entra21.ExerciciosForTry/Program.cs
@@ -2,7 +2,8 @@
 
 Console.WriteLine(@"--------Menu-------
 1- Exercício 01
-2- Exercício 05
+2- Exercício 02
+3- Exercício 03
 ");
 
 Console.Write("Digite a opção desejada: ");
@@ -18,4 +19,9 @@ else if (opcaoDesejada == 2)
 {
     Exercicio02 exercicio02 = new Exercicio02();
     exercicio02.Executar();
+}
+else if (opcaoDesejada == 3)
+{
+    Exercicio03 exercicio03 = new Exercicio03();
+    exercicio03.Executar();
 }
