b7c8b634dc7463a928f1a62f6c943f90cb7dd5ae
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-20 19:58:54 -0300

    Exercicio04 Completo Exercicio 04 concluido em sala de aula

---
+++ Entra21.ExerciciosVetor/Exercicio04.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio04
+    {
+        public void Executar()
+        {
+            // Crie um vetor para armazenar as notas de uma disciplina. A disciplina deve conter 4 notas.
+            // Solicite para o usuário o nome e as quatro notas, armazenando as notas em um vetor, realize a média destas notas e apresente.
+
+            double[] notas = new double[4];
+
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
+
+            var media = 0.0;
+
+            for (int i = 0; i < notas.Length; i++)
+            {
+                while (notas[i] <= 0)
+                {
+                    try
+                    {
+                        Console.Write("Nota: ");
+                        notas[i] = Convert.ToDouble(Console.ReadLine());
+
+                        if (notas[i] <= 0)
+                        {
+                            Console.WriteLine("Nota menor ou igual a 0 não é válida.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Digite uma nota válida.");
+                    }
+                }
+
+                media = media + notas[i];
+
+            }
+
+            Console.WriteLine(@"
+Média: " + media / 4);
+
+
+
+
+        }
+    }
+}
--- Entra21.ExerciciosVetor/Program.cs
+++ Entra21.ExerciciosVetor/Program.cs
@@ -4,18 +4,19 @@ Console.WriteLine(@"--------Menu-------
 1- Exercício 01
 1- Exercício 02
 3- Exercício 03
+4- Exercício 04
 ");
 
 var opcaoDesejada = 0;
 
-while ((opcaoDesejada <= 0) || (opcaoDesejada > 3))
+while ((opcaoDesejada <= 0) || (opcaoDesejada > 4))
 {
     try
     {
         Console.WriteLine("Digite a opção desejada: ");
         opcaoDesejada = Convert.ToInt32(Console.ReadLine());
 
-        if ((opcaoDesejada <= 0) || (opcaoDesejada > 3))
+        if ((opcaoDesejada <= 0) || (opcaoDesejada > 4))
         {
             Console.WriteLine(@"
 Apenas um número do menu
@@ -45,4 +46,9 @@ else if (opcaoDesejada == 3)
 {
     Exercicio03 exercicio03 = new Exercicio03();
     exercicio03.Executar();
+}
+else if (opcaoDesejada == 4)
+{
+    Exercicio04 exercicio04 = new Exercicio04();
+    exercicio04.Executar();
 }
