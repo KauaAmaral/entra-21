8bf48cbdcc21a0772c4bb9944256c6ff0084faec
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-20 19:06:01 -0300

    Exercicio01 Completo Exercicio 01 Concluido em sala de aula

--- Entra21.ExerciciosVetor/Exercicio01.cs
+++ Entra21.ExerciciosVetor/Exercicio01.cs
@@ -10,7 +10,39 @@ namespace Entra21.ExerciciosVetor
     {
         public void Executar()
         {
+            // 1. Crie um vetor para guardar 16 números inteiros.
+            // Preencha todas as posições com números e apresente a soma destes números.
 
+            int[] numeros = new int[16];
+            var soma = 0.0;
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                var numeroString = true;
+
+                while (numeroString == true)
+                {
+                    try
+                    {
+                        Console.Write("Digite um número: ");
+                        numeros[i] = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+
+                        soma = soma + numeros[i];
+
+                        numeroString = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Apenas números");
+
+                        numeroString = true;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Soma: {soma}");
         }
     }
 }
