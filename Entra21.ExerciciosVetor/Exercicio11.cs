07c3ae4e55447906c8227b63979785ef3a4c81d8
Kauã Amaral <105084991+KauaAmaral@users.noreply.github.com>
2022-05-23 11:49:56 -0300

    Exercicio11 SemiCompleto
    Adicionar Try no código
    Alinhar o código

---
+++ Entra21.ExerciciosVetor/Exercicio11.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio09
+    {
+        public void Executar()
+        {
+        
+        // Crie um vetor e solicite os números. Crie outros dois vetores, um deles irá armazenar
+        // somente os números pares do vetor 1, o outro irá armazenar somente os números ímpares do vetor 1.
+        // Obrigatório utilização de for para preencher o vetor 2 e 3.
+        
+		    var quantidadePares = 0;
+				var quantidadeImpares = 0;
+		 double[] numeros = new double[10];
+		 double[] numerosImpares = new double[10];
+		 double[] numerosPares = new double[10];
+		
+
+            for (var i = 0; i < numeros.Length; i++)
+            {
+                        Console.Write("Numeros: ");
+                        numeros[i] = Convert.ToDouble(Console.ReadLine());
+                        Console.Clear();
+				
+                        if (numeros[i] % 2 == 0)
+                        {
+							
+                            numerosPares[quantidadePares] = numeros[i];
+							quantidadePares = quantidadePares + 1;
+                        }
+                        else if (numeros[i] % 2 != 0)
+                        {
+							
+                           numerosImpares[quantidadeImpares] = numeros[i];
+						   quantidadeImpares = quantidadeImpares + 1;
+                        }
+                }
+            
+            for (var i = 0; i < quantidadeImpares; i++)
+            {
+				Console.WriteLine("Impares: " + numerosImpares[i]);
+            }
+		for (var i = 0; i < quantidadePares; i++)
+		{
+			   Console.WriteLine("Pares: " + numerosPares[i]);
+		}
+    
+  }
+ }
+}
+    
