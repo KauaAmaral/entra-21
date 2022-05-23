0e719fd9b6709dd9603791158cb8caa5f1127210
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-23 19:07:03 -0300

    ExerciciosVetor Completo Exercicios da lista vetor todos completos

--- Entra21.ExerciciosVetor/Exercicio10.cs
+++ Entra21.ExerciciosVetor/Exercicio10.cs
@@ -16,15 +16,30 @@ namespace Entra21.ExerciciosVetor
             // - E continua até terminar o preenchimento do vetor 2.
             // Obrigatório utilização de for para preencher o vetor 2.
 
-            var quantidade = 8;
-            int[] numerosSolicitados = new int[quantidade];
-            int[] numerosSomados = new int[quantidade];
+            var quantidade = 7;
+            int[] numerosSolicitados = new int[8];
+            int[] numerosSomados = new int[8];
 
             for (var i = 0; i < numerosSolicitados.Length; i++)
             {
+                var numeroLetra = true;
+                while (numeroLetra == true)
+                {
+                    try
+                    {
                 Console.Write("Numero: ");
                 numerosSolicitados[i] = Convert.ToInt32(Console.ReadLine());
                 Console.Clear();
+
+                        numeroLetra = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Valor inválido.");
+                        numeroLetra = true;
+                    }
+                }
             }
 
             for (var i = 0; i < numerosSolicitados.Length; i++)
