f7fc9830b5c8cefa429da9e211f69cb62a5c91a1
Kauã Amaral <105084991+KauaAmaral@users.noreply.github.com>
2022-05-23 11:18:20 -0300

    Adicionado Exercicio10
    Verificar com professor qual é o erro

---
+++ Entra21.ExerciciosVetor/Exercicio10.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosVetor
+{
+    internal class Exercicio09
+    {
+        public void Executar()
+        {
+        // Criei um vetor e solicite 8 números. Crie outro vetor preenchendo ele da seguinte forma.
+        // - Primeira posição do vetor 2 recebe a soma da última posição do vetor 1 e a primeira posição do vetor 1.
+        // - Segunda posição do vetor 2 recebe a soma da primeira posição do vetor 1 e a segunda posição do vetor 1.
+        // - E continua até terminar o preenchimento do vetor 2.
+        // Obrigatório utilização de for para preencher o vetor 2.
+		
+		var quantidade = 8;
+		int[] numerosSolicitados = new int[quantidade];
+		int[] numerosSomados = new int[quantidade];
+		
+		for (var i = 0; i < numerosSolicitados.Length; i++)
+		{
+			Console.Write("Numero: ");
+			numerosSolicitados[i] = Convert.ToInt32(Console.ReadLine());
+			Console.Clear();
+		}
+		
+		for (var i = 0; i < numerosSolicitados.Length; i++)
+		{
+			numerosSomados[i] = numerosSolicitados[i] + numerosSolicitados[quantidade];
+			quantidade = quantidade - 1;
+		}
+		
+		for (var i = 0; i < numerosSolicitados.Length; i++)
+		{
+			Console.WriteLine("Numeros Solicitados: " + numerosSolicitados[i] +
+							 "\nSoma: " + numerosSomados[i]);
+		}
+  }
+ }
+}
