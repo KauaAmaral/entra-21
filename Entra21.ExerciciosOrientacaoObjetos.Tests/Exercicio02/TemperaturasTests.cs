4eed79a6b6161ccefd0b195929729f829850c451
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-27 20:10:59 -0300

    Refatorar

---
+++ Entra21.ExerciciosOrientacaoObjetos.Tests/Exercicio02/TemperaturasTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Tests.Exercicio02
+{
+    internal class TemperaturasTests
+    {
+
+
+
+    }
+}
