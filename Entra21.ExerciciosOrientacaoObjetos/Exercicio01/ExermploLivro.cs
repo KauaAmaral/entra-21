a87c2afea0e7e7d21595b6d45691fa94fed508ef
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-26 22:05:56 -0300

    Exercicio01 / Livro Terminar Exercicio no proximo dia

---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio01/ExermploLivro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class ExermploLivro
+    {
+        public void Executar()
+        {
+            var livro = new Livro();
+            livro.Autor = "Klebin Sena";
+            livro.Titulo = "C Sharp";
+            livro.CodigoIsbn = "1234567899";
+            livro.IdiomaOriginal = "Português";
+            livro.DataLancamento = new DateTime(2014, 04, 10);
+            livro.QuantidadePaginas = 198;
+            livro.Releituras = 0;
+            livro.QuantidadePaginasLidas = 120;
+
+        }
+    }
+}
+
---
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio01/Livro.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio01
+{
+    public class Livro
+    {
+        //Crie uma classe chamada Livro para armazenar as seguintes informações
+        //● Título
+        //● Autor
+        //● Código ISBN
+        //● Idioma original
+        //● Data de lançamento
+        //● Quantidade de páginas
+        //● Quantidade de páginas lidas
+        //● Quantidade de releituras
+
+        //Crie os seguintes métodos:
+        //● ApresentarTituloAutor
+        //● ApresentarQuantidadePaginasParaLer
+        //● ApresentarQuantidadePaginasLidasNoTotal
+
+        //Criar testes unitários validando os métodos criados acima
+
+        public string Titulo, Autor, CodigoIsbn, IdiomaOriginal;
+        public int QuantidadePaginas, QuantidadePaginasLidas, Releituras;
+        public DateTime DataLancamento;
+
+        public void ApresentarTituloAutor()
+        {
+            Console.WriteLine(@$"Título: {Titulo}
+Altor: {Autor}");
+        }
+
+        public int ApresentarQuantidadePaginasParaLer()
+        {
+            var quantidadePaginasParaLer = QuantidadePaginas - QuantidadePaginasLidas;
+
+            return quantidadePaginasParaLer;
+        }
+
+        public int ApresentarQuantidadePaginasLidasNoTotal()
+        {
+            var quantidadePaginasLidasNoTotal = QuantidadePaginasLidas * Releituras;
+
+            if (Releituras <= 0)
+                quantidadePaginasLidasNoTotal = QuantidadePaginas;
+
+            return quantidadePaginasLidasNoTotal;
+        }
+
+
+
+
+
+    }
+}
