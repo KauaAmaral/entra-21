47429b1b28713d2ce797614083d8381a5f30b843
Kauã Amaral <kauaamaral10@gmail.com>
2022-05-27 21:42:21 -0300

    Exercicio02 Completo Exercicio 02 concluido em sala de aula

--- Entra21.ExerciciosOrientacaoObjetos.Tests/Exercicio02/TemperaturasTests.cs
+++ Entra21.ExerciciosOrientacaoObjetos.Tests/Exercicio02/TemperaturasTests.cs
@@ -22,9 +22,78 @@ namespace Entra21.ExerciciosOrientacaoObjetos.Tests.Exercicio02
 
             var calcularFahrenheitParaCelsius = temperatura.CalcularFahrenheitParaCelsius();
 
-            var resultadoFinal = (temperatura.Temperatura - 32) * 1.8;
+            var temperaturaFinal = (temperatura.Temperatura - 32) * 1.8;
 
-            calcularFahrenheitParaCelsius.Should().Be(resultadoFinal);
+            calcularFahrenheitParaCelsius.Should().Be(temperaturaFinal);
         }
+
+        public void Validar_Calcular_Kelvin_Para_Celsius()
+        {
+            Temperaturas temperatura = new Temperaturas();
+            temperatura.TemperaturaDeOrigem = "kelvin";
+            temperatura.TemperaturaDeDestino = "celsius";
+            temperatura.Temperatura = 300;
+
+            var calcularKelvinParaCelsius = temperatura.CalcularKelvinParaCelsius();
+
+            var temperaturaFinal = temperatura.Temperatura - 273.15;
+
+            calcularKelvinParaCelsius.Should().Be(temperaturaFinal);
+        }
+
+        public void Validar_Calcular_Celsius_Para_Kelvin()
+        {
+            Temperaturas temperatura = new Temperaturas();
+            temperatura.TemperaturaDeOrigem = "celsius";
+            temperatura.TemperaturaDeDestino = "kelvin";
+            temperatura.Temperatura = 300;
+
+            var CalcularCelsiusParaKelvin = temperatura.CalcularCelsiusParaKelvin();
+
+            var temperaturaFinal = temperatura.Temperatura + 273.15;
+
+            CalcularCelsiusParaKelvin.Should().Be(temperaturaFinal);
+        }
+
+        public void Validar_Calcular_Fahrenheit_Para_Kelvin()
+        {
+            Temperaturas temperatura = new Temperaturas();
+            temperatura.TemperaturaDeOrigem = "fahrenheit";
+            temperatura.TemperaturaDeDestino = "kelvin";
+            temperatura.Temperatura = 300;
+            
+            var calcularFahrenheitParaKelvin = temperatura.CalcularFahrenheitParaKelvin();
+
+            var temperaturaFinal = (temperatura.Temperatura - 32) * 5 / 9 + 273.15;
+
+            calcularFahrenheitParaKelvin.Should().Be(temperaturaFinal);
+        }
+
+        public void Validar_Calcular_Kelvin_Para_Fahrenheit()
+        {
+            Temperaturas temperatura = new Temperaturas();
+            temperatura.TemperaturaDeOrigem = "kelvin";
+            temperatura.TemperaturaDeDestino = "fahrenheit";
+            temperatura.Temperatura = 300;
+
+            var calcularKelvinParaFahrenheit = temperatura.CalcularKelvinParaFahrenheit();
+
+            var temperaturaFinal = (temperatura.Temperatura - 273.15) * 5 / 9 + 32;
+
+            calcularKelvinParaFahrenheit.Should().Be(temperaturaFinal);
+        }
+
+        public void Validar_Calcular_CelsiusPara_Fahrenheit()
+        {
+            Temperaturas temperatura = new Temperaturas();
+            temperatura.TemperaturaDeOrigem = "celsius";
+            temperatura.TemperaturaDeDestino = "fahrenheit";
+            temperatura.Temperatura = 300;
+
+            var calcularCelsiusParaFahrenheit = temperatura.CalcularCelsiusParaFahrenheit();
+
+            var temperaturaFinal = temperatura.Temperatura * 1.8 + 32;
+        }
+
     }
 }
