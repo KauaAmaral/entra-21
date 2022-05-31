ba6a747241f24bb9dec3caaeceaefdcae2d22cd1
Kauã Amaral <105084991+KauaAmaral@users.noreply.github.com>
2022-05-31 10:34:43 -0300

    Exercicio04 Semi completo / Falta Tests
    Exercicio04 Semi completo
    só falta os tests

--- Entra21.ExerciciosOrientacaoObjetos/Exercicio04/ExemploFarmacia.cs
+++ Entra21.ExerciciosOrientacaoObjetos/Exercicio04/ExemploFarmacia.cs
@@ -14,19 +14,19 @@ namespace Entra21.ExerciciosOrientacaoObjetos.Exercicio04
             farmacia.RazaoSocial = "Farmacia Popular";
             farmacia.Cnpj = "12.345.678/0001-90";
 
-            farmacia.Categoria[1] = "Curativos";
-            farmacia.Categoria[2] = "Remedios";
-            farmacia.Categoria[3] = "Perfumes";
+            farmacia.Categoria[0] = "Curativos";
+            farmacia.Categoria[1] = "Remedios";
+            farmacia.Categoria[2] = "Perfumes";
 
-            farmacia.NomeProduto[1] = "Bandaid";
+            farmacia.NomeProduto[0] = "Bandaid";
+            farmacia.CategoriaProduto[0] = farmacia.Categoria[0];
+            farmacia.PrecoProduto[0] = 4.99;
+            farmacia.NomeProduto[1] = "DorFlex";
             farmacia.CategoriaProduto[1] = farmacia.Categoria[1];
-            farmacia.PrecoProduto[1] = 4.99;
-            farmacia.NomeProduto[2] = "DorFlex";
+            farmacia.PrecoProduto[1] = 12.90;
+            farmacia.NomeProduto[2] = "Paracetamol";
             farmacia.CategoriaProduto[2] = farmacia.Categoria[2];
-            farmacia.PrecoProduto[2] = 12.90;
-            farmacia.NomeProduto[3] = "Paracetamol";
-            farmacia.CategoriaProduto[3] = farmacia.Categoria[2];
-            farmacia.PrecoProduto[3] = 10.89;
+            farmacia.PrecoProduto[2] = 10.89;
 
 
         }
