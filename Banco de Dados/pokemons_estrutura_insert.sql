ac28b92333883fe0d2ff29bc98939ebc3576d63b
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-08 19:44:13 -0300

    Pokemons da Lista02 Completo Concluido os pokemons da lista02

--- Banco de Dados/pokemons_estrutura_insert.sql
+++ Banco de Dados/pokemons_estrutura_insert.sql
@@ -736,4 +736,67 @@ INSERT INTO pokemons (nome, codigo, categoria, descricao, altura, peso, hp, ataq
 ('Diancie', '719', 'Jewel', 'A sudden transformation of Carbink,its pink, glimmering body is said to bethe loveliest sight in the whole world.', 0.7, 8.8, 2, 5, 6, 5, 6, 3),
 ('Hoopa', '720', 'Mischief', 'In its true form, it possesses a huge amount of power. Legends of its avarice tell how it once carried off an entire castle to gain the treasure hidden within.', 0.5, 9.0, 4, 6, 3, 6, 5, 4);
 
-SELECT
+--Tabela de pokemons:
+--Utilize o arquivo pokemons_estrutura_insert.sql. Execute este arquivo antes de começar a resolver os exercícios
+--abaixo.
+
+--1. Selecione todas as colunas.
+SELECT id, nome, codigo, categoria, altura, peso, hp, ataque, defesa, especial_ataque, especial_defesa,
+	velocidade, descricao 
+	FROM pokemons;
+
+--2. Selecione o ataque, ataque especial, defesa e defesa especial.
+SELECT ataque, especial_ataque, defesa, especial_defesa
+	FROM pokemons;
+
+--3. Selecione nome, categoria e ataque ordenado pelo ataque em ordem crescente.
+SELECT nome, categoria, ataque
+	FROM pokemons
+	ORDER BY ataque ASC;
+
+--4. Selecione altura, peso, com o cálculo do imc.
+ALTER TABLE pokemons ADD imc FLOAT;
+
+UPDATE pokemons SET imc = peso / (altura * altura);
+
+SELECT altura, peso, imc 
+	FROM pokemons;
+
+--5. Selecione altura, peso, com o cálculo do imc ordenando o imc em ordem decrescente.
+SELECT altura, peso, imc
+	FROM pokemons
+	ORDER BY imc DESC;
+
+--6. Selecione nome e o tamanho do nome em ordem decrescente pelo nome.
+SELECT nome, LEN(nome) AS 'Tamanho do nome'
+	FROM pokemons
+	ORDER BY nome DESC;
+
+--7. Selecione nome, descrição, quando o nome contiver mais que 10 caracteres.
+SELECT nome, descricao
+	FROM pokemons
+	WHERE LEN(nome) > 10;
+
+--8. Selecione nome, categoria, e ataque do pokemon que contém o menor valor de ataque.
+SELECT nome, categoria, ataque
+	FROM pokemons
+	WHERE ataque = (SELECT MIN(p.ataque)
+	FROM pokemons p);
+
+--9. Selecione o ataque, ataque especial, nome, defesa e defesa especial ordenando pelo ataque.
+SELECT ataque, especial_ataque, nome, defesa, especial_defesa
+	FROM pokemons
+	ORDER BY ataque ASC;
+
+--10. Selecione a média dos ataques.
+SELECT SUM(ataque) / COUNT(ataque) AS 'Media dos Ataques'
+	FROM pokemons;
+
+--11. Selecione a somatória dos ataques.
+SELECT SUM(ataque) AS 'Soma dos Ataques'
+	FROM pokemons;
+
+--12. Selecione a média dos ataques especiais quando o nome do pokemon começar com ‘P’.
+SELECT SUM(especial_ataque) / COUNT(especial_ataque) AS 'Medias dos Ataques Especiais'
+	FROM pokemons
+	WHERE nome LIKE 'P%';
