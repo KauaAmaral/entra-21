21da0a923bfcaeecb2a674b308f96bac05edfc3b
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-14 22:07:38 -0300

    Exemplo Banco de Dados Com C# adicionado exemplo de banco de dados

---
+++ Entra21.BancoDados01.Ado.Net/DataBase/Conexao.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace Entra21.BancoDados01.Ado.Net.DataBase
+{
+    internal class Conexao
+    {
+        public SqlConnection Conectar()
+        {
+            // Instanciado um objeto da classe SqlConnection, que permitira fazer selects, inserts, updates, deletes, etc 
+            SqlConnection conexao = new SqlConnection();
+
+            // String quem contem o caminho para o banco de dados, o que permitira conectar ao banco de dados
+            var connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\kaua.amaral\\source\\repos\\KauaAmaral\\entra-21\\Banco de Dados\\ExemploBancoDados01AdoNet.mdf\";Integrated Security=True;Connect Timeout=30";
+
+            // Definir o caminho da conexao para o SqlConnection
+            conexao.ConnectionString = connectionString;
+
+            // Abrir a conexao com o banco de dados
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
---
+++ Entra21.BancoDados01.Ado.Net/Services/TipoPersonagemService.cs
@@ -0,0 +1,87 @@
+﻿using Entra21.BancoDados01.Ado.Net.DataBase;
+using Entra21.BancoDados01.Ado.Net.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Entra21.BancoDados01.Ado.Net.Services
+{
+    // Dois pontos seguido de uma interface, quer dizer que a classe sera obrigada a implementar os metodos(contratos) estabelecidos na interface
+    internal class TipoPersonagemService : ITipoPersonagemService
+    {
+        public void Apagar(int id)
+        {
+            // Conectar com o bd
+            var conexao = new Conexao().Conectar();
+
+            // Criar comando para executar o delete
+            var comando = conexao.CreateCommand();
+
+            // Definido o comando para apagar o registro
+            comando.CommandText = "DELETE FROM tipos_personagens WHERE id = " + id;
+
+            // Executando o comando para apagar o registro
+            comando.ExecuteNonQuery();
+
+            // Fechar conexao com o bd
+            comando.Connection.Close();
+        }
+
+        public void Cadastrar(TipoPersonagem tipoPersonagem)
+        {
+            // Sera descrito a iplementacao do contrato definido na interface
+
+            // Criado conexao com o BancoDeDados e aberta a conexa
+            var conexao = new Conexao().Conectar();
+
+            // Criar o comando para executar no banco de dados
+            SqlCommand comando = conexao.CreateCommand();
+
+            // Especificado o comando que sera executado
+            comando.CommandText = "INSERT INTO tipos_personagens (tipo) VALUES('" +
+                tipoPersonagem.Tipo + "')";
+
+            // Executando o comando de insert na tabela de tipos personagens
+            comando.ExecuteNonQuery();
+
+            conexao.Close();
+        }
+
+        public List<TipoPersonagem> ObterTodos()
+        {
+            // Criado conexao com o BancoDeDados e aberta a conexa
+            var conexao = new Conexao().Conectar();
+
+            var comando = conexao.CreateCommand();
+
+            comando.CommandText = "SELECT id, tipo FROM tipos_personagens";
+
+            // Criando tabela de memoria para posteriormente carregar os registros obtidos com o SELECT
+            var tabelaEmMemoria = new DataTable();
+
+            // Carregado a tabela em memoria os registros encontrados com o comando SELECT
+            tabelaEmMemoria.Load(comando.ExecuteReader());
+
+            // Criado uma lista para armazenar os registros da consulta do SELECT
+            var tiposPersonagens = new List<TipoPersonagem>();
+
+            // Percorrido cada um dos registros da tabelaEmMemoria(registros do SELECT)
+            for (int i = 0; i < tabelaEmMemoria.Rows.Count; i++)
+            {
+                // Obtido o registro da linha que esta sendo percorrida
+                var linha = tabelaEmMemoria.Rows[i];
+
+                // Instanciando objeto da classe TipoPersonagem preenchendo com as informacoes do SELECT, da linha que esta sendo percorrida
+                var tipoPersonagem = new TipoPersonagem();
+                tipoPersonagem.Id = Convert.ToInt32(linha["id"].ToString());
+                tipoPersonagem.Tipo = linha["tipo"].ToString();
+
+                // Adicionado tipoPersonagens instanciado na linha de TiposPersonagens
+                tiposPersonagens.Add(tipoPersonagem);
+            }
+            // Fechando a conexao com o banco de dados
+            comando.Connection.Close();
+
+            return tiposPersonagens;
+        }
+    }
+}
