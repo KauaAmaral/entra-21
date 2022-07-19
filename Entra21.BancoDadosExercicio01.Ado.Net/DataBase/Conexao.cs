0322ad9654f23de396ec03ab41dbe5f29850b938
Kauã Amaral <kauaamaral10@gmail.com>
2022-07-18 22:10:49 -0300

    Exercicio Criado Terminar em casa

---
+++ Entra21.BancoDadosExercicio01.Ado.Net/DataBase/Conexao.cs
@@ -0,0 +1,18 @@
+﻿namespace Entra21.BancoDadosExercicio01.Ado.Net.DataBase
+{
+    internal class Conexao
+    {
+        public SqlConnection Conectar()
+        {
+            SqlConnection conexao = new SqlConnection();
+
+            var connectionString = @"";
+
+            conexao.ConnectionString = connectionString;
+
+            conexao.Open();
+
+            return conexao;
+        }
+    }
+}
